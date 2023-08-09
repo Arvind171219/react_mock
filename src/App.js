85557f4447e1cbf315d596e0948dba1dc887fd78
Arvind kumar <arvindkr40882@gmail.com>
2023-08-09 18:55:53 +0530

    Counter-App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Components/Home';
-import Contact from './Components/Contact';
-import About from './Components/About';
+import Counter from './Counter'
+import './Style.css';
 
-function App() {
+export default function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/contact" component={Contact} />
-        </Switch>
-      </div>
-    </Router>
+    <div className='App'>
+      <Counter/>
+    </div>
   )
 }
-export default App;
