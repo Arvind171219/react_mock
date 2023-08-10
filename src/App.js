74b94427b3dd1fa400794a58eb0a37ce0da6e427
Arvind kumar <arvindkr40882@gmail.com>
2023-08-10 18:28:05 +0530

    Bird- List

--- src/App.js
+++ src/App.js
@@ -1,44 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CountrySelector from './CountrySelector';
+import React from 'react';
+import BirdList from './BirdList';
 
 function App() {
-  const [countryCode, setCountryCode] = useState('');
-  const [countryData, setCountryData] = useState({});
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (countryCode) {
-      setLoading(true);
-      axios.get(`https://disease.sh/v3/covid-19/countries/${countryCode}`)
-        .then(response => {
-          setCountryData(response.data);
-          setLoading(false);
-        })
-        .catch(error => {
-          console.error('Error fetching COVID-19 data:', error);
-          setLoading(false);
-        });
-    }
-  }, [countryCode]);
-
-  const handleCountrySelect = (selectedCountryCode) => {
-    setCountryCode(selectedCountryCode);
-  };
-
   return (
     <div className="App">
-      <h1>COVID-19 Cases Tracker</h1>
-      <CountrySelector onSelect={handleCountrySelect} />
-      {loading && <p>Loading...</p>}
-      {countryData && Object.keys(countryData).length > 0 && (
-        <div>
-          <h2>{countryData.country}</h2>
-          <p>Cases: {countryData.cases}</p>
-          <p>Deaths: {countryData.deaths}</p>
-          <p>Recovered: {countryData.recovered}</p>
-        </div>
-      )}
+      <BirdList />
     </div>
   );
 }
