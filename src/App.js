97d731603ca79b994f283866b7c4275a8f43a773
Arvind kumar <arvindkr40882@gmail.com>
2023-08-11 11:43:39 +0530

    Appending String and Undo

--- src/App.js
+++ src/App.js
@@ -1,69 +1,32 @@
-import React,{useState} from "react";
+import React from "react";
+import { useState } from "react";
 
-const APIURL = "https://jsonplaceholder.typicode.com/users";
-function App() {
- const [userList, setUserList] = useState([]);
- const [originalUserList, setOriginalUserList] = useState([]);
- const [sortlevel, setSortlevel] = useState('');
-  const getUsers = async () => {
-    const response = await fetch(APIURL);
-    const data = await response.json();
-    // console.log(data, typeof data);
-    const users = [];
-    for(let i = 0; i < data.length; i++){
-      users.push(data[i]);
-    }
-    setUserList(users);
-    setOriginalUserList(users);
-    setSortlevel("1");
-    //write code here
-    //render the list of users
+export default function App() {
+  const [data, setData] = useState("");
+  const [history, setHistory] = useState([]);
+
+  console.log(history, data);
+  function handleAppend(e) {
+    setData(e.target.value);
   }
-  const sortList = () => {
-      //write code here
-      // sort the user list by name's length
-      // on first click list will sorted in assending order
-      if(sortlevel === '1'){
-        const userNameinAssending = userList.sort((a,b)=> a.name.length - b.name.length);
-        setUserList(userNameinAssending);
-        console.log(originalUserList);
-        setSortlevel('2');
-      }
-      // on second click list will be sorted in descending order
-      if(sortlevel === '2'){
-        const userNameinDescendingOrder = userList.sort((a,b)=> b.name.length - a.name.length);
-        setUserList(userNameinDescendingOrder);
-        console.log(originalUserList);
-        setSortlevel('3');
-      }
-      // on third click default list will be rendered
-      if(sortlevel === '3'){
-        console.log(originalUserList);
-        setUserList(originalUserList);
-        setSortlevel('1');
-      }
-      // on fourth click again start form step 1
+  function ape() {
+    if (!data) return;
+    setHistory((hist) => [...hist, data]);
+    setData("");
+    // setVal(true);
   }
-  
+  function und() {
+    setHistory([...history].slice(0, -1));
+    setData("");
+  }
   return (
-    <main>
-      <h1>User List</h1>
-      <div>
-        <button onClick={getUsers}>Get Users</button>
-        <button onClick={sortList}>Sort list </button>
-      </div>
-      <ul>
-        {/*render the list of the users */
-        sortlevel &&
-         userList.map((user) =>(
-           <li key = {user.id}>
-           {user.name} 
-           </li>
-         ))
-        }
-      </ul>
-    </main>
-  )
+    <div>
+      <input type="text" value={data} onChange={handleAppend}></input>
+      <button onClick={ape}>Append</button>
+      <button onClick={und}>Undo</button>
+      {history.map((item) => (
+        <span>{item}</span>
+      ))}
+    </div>
+  );
 }
-
-export default App;
