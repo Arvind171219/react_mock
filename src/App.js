e37f9bb3b11f26b6f3c5738b39c8e5af099ff7c4
Arvind kumar <arvindkr40882@gmail.com>
2023-08-11 11:53:17 +0530

    Add -Task with edit and update

--- src/App.js
+++ src/App.js
@@ -1,32 +1,80 @@
-import React from "react";
-import { useState } from "react";
+import { useReducer } from 'react';
+import AddTask from './AddTask.js';
+import TaskList from './TaskList.js';
 
-export default function App() {
-  const [data, setData] = useState("");
-  const [history, setHistory] = useState([]);
+function tasksReducer(tasks, action) {
+  switch (action.type) {
+    case 'added': {
+      return [...tasks, {
+        id: action.id,
+        text: action.text,
+        done: false
+      }];
+    }
+    case 'changed': {
+      return tasks.map(t => {
+        if (t.id === action.task.id) {
+          return action.task;
+        } else {
+          return t;
+        }
+      });
+    }
+    case 'deleted': {
+      return tasks.filter(t => t.id !== action.id);
+    }
+    default: {
+      throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
 
-  console.log(history, data);
-  function handleAppend(e) {
-    setData(e.target.value);
+export default function TaskApp() {
+  const [tasks, dispatch] = useReducer(
+    tasksReducer,
+    initialTasks
+  );
+
+  function handleAddTask(text) {
+    dispatch({
+      type: 'added',
+      id: nextId++,
+      text: text,
+    });
   }
-  function ape() {
-    if (!data) return;
-    setHistory((hist) => [...hist, data]);
-    setData("");
-    // setVal(true);
+
+  function handleChangeTask(task) {
+    dispatch({
+      type: 'changed',
+      task: task
+    });
   }
-  function und() {
-    setHistory([...history].slice(0, -1));
-    setData("");
+
+  function handleDeleteTask(taskId) {
+    dispatch({
+      type: 'deleted',
+      id: taskId
+    });
   }
+
   return (
-    <div>
-      <input type="text" value={data} onChange={handleAppend}></input>
-      <button onClick={ape}>Append</button>
-      <button onClick={und}>Undo</button>
-      {history.map((item) => (
-        <span>{item}</span>
-      ))}
-    </div>
+    <>
+      <h1>Prague itinerary</h1>
+      <AddTask
+        onAddTask={handleAddTask}
+      />
+      <TaskList
+        tasks={tasks}
+        onChangeTask={handleChangeTask}
+        onDeleteTask={handleDeleteTask}
+      />
+    </>
   );
 }
+
+let nextId = 3;
+const initialTasks = [
+  { id: 0, text: 'Visit Kafka Museum', done: true },
+  { id: 1, text: 'Watch a puppet show', done: false },
+  { id: 2, text: 'Lennon Wall pic', done: false }
+];
